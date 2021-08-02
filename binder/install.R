5647ac5950f4845ef16a599bce2d278586c46946
monaghaa <33913248+monaghaa@users.noreply.github.com>
2021-08-02 16:41:53 -0600

    Create install.R

---
+++ binder/install.R
@@ -0,0 +1,17 @@
+# Data analysis
+install.packages("tidyverse")
+# R Markdown documents converter
+install.packages("rmarkdown")
+# HTTP tools
+install.packages("httr")
+# Shiny
+install.packages("shinydashboard")
+# Interactive maps
+install.packages('leaflet')
+#others
+install.packages('ggstatsplot')
+install.packages('psych')
+install.packages('knitr')
+install.packages('broom')
+install.packages('fivethirtyeight')
+install.packages('ggpubr')
